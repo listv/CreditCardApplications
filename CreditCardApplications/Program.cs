52fe31b2e40e4afa55277c48a13f826a0047ff65
akuziakov <akuziakov@gmail.com>
2019-04-16 19:47:17 +0300

    Add project files.

---
+++ CreditCardApplications/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreditCardApplications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
