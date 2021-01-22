a5080f55169add395aae2e535585301104c22cac
Oleksandr Kuziakov <okuzia@softserveinc.com>
2021-01-22 19:22:19 +0200

    Add exclamation sign 1

--- CreditCardApplications/Program.cs
+++ CreditCardApplications/Program.cs
@@ -6,7 +6,7 @@ namespace CreditCardApplications
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
